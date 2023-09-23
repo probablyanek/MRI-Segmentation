a9d0ce3069c001c874e9331ec7fa49086f2988e2
unknown <mjajdnmx@gmail.com>
2023-09-24 02:04:35 +0530

    First commit

---
+++ model/main.py
@@ -0,0 +1,11 @@
+from ultralytics import YOLO
+import torch
+
+if __name__ == '__main__':
+
+    print(torch.cuda.is_available())
+    # Load a model
+    model = YOLO('yolov8-seg.yaml')  
+    # Train the model
+    # results = model.train(data='config.yaml', epochs=100, imgsz=500, batch = 4)
+    results = model.train(data="config.yaml", epochs=100, imgsz=500, batch = 8)
